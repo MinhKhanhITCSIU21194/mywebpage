32d7c1b06f31bda5e6b1c8797be728699331bb57
Tohsaka <peterhuy2906@gmail.com>
2024-05-26 16:20:06 +0700

    CreateAccount ne

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import ProdBody from './components/Pages/ProdBody';
 import Homepage from './components/Pages/Homepage';
@@ -7,28 +6,25 @@ import IntroductionComponent from './components/Pages/Introduction';
 import Education from './components/Pages/Education';
 import EducourseBody from './components/Pages/EducourseBody';
 import Login from './components/Pages/Login';
+import CreateAccount from './components/Pages/CreateAccount';
 
 function App() {
   return (
-<div>
-    <Routes>
-      <Route path="/" element={<Homepage/>}/>
-      <Route path="/products" element={<ProdBody/>}/>
-      <Route path="/introduction" element={<IntroductionComponent/>}/>
-      <Route path="/education" element={<Education/>}/>
-      <Route path="/education1" element={<EducourseBody ID='1' />}/>
-      <Route path="/education2" element={<EducourseBody ID='2' />}/>
-      <Route path="/education3" element={<EducourseBody ID='3' />}/>
-      <Route path="/education4" element={<EducourseBody ID='4' />}/>
-      <Route path="/education5" element={<EducourseBody ID='5' />}/>
-      <Route path="/Login" element={<Login/>}/>
-      
-    </Routes>
-
-   </div>  
-  
-     
-
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/products" element={<ProdBody/>}/>
+        <Route path="/introduction" element={<IntroductionComponent/>}/>
+        <Route path="/education" element={<Education/>}/>
+        <Route path="/education1" element={<EducourseBody ID='1' />}/>
+        <Route path="/education2" element={<EducourseBody ID='2' />}/>
+        <Route path="/education3" element={<EducourseBody ID='3' />}/>
+        <Route path="/education4" element={<EducourseBody ID='4' />}/>
+        <Route path="/education5" element={<EducourseBody ID='5' />}/>
+        <Route path="/login" element={<Login/>}/> 
+        <Route path="/create-account" element={<CreateAccount/>}/> 
+      </Routes>
+    </div>  
   );
 }
 
